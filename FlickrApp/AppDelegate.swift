8ba90aee256b691f66e24324799d3ef9341f57a8
sharon mcdermott <sharonmcdermott@me.com>
2016-11-23 10:28:19 -0500

    Set theme color for app.

--- FlickrApp/AppDelegate.swift
+++ FlickrApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+let themeColor = UIColor(red: 0.01, green: 0.41, blue: 0.22, alpha: 1.0)
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window?.tintColor = themeColor
         return true
     }
 
