167d0d7167127729a3703f88993c6503778a8517
sharon mcdermott <sharonmcdermott@me.com>
2016-11-23 11:21:37 -0500

    Created FlickrPhotosViewController.swift

---
+++ FlickrApp/FlickrPhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickrApp
+//
+//  Created by sharon mcdermott on 11/23/16.
+//  Copyright © 2016 sharon mcdermott. All rights reserved.
+//
+
+import UIKit
+
+final class FlickrPhotosViewController: UICollectionViewController {
+    // MARK: - Properties
+    fileprivate let reuseidentifier = "FlickrCell"
+}
