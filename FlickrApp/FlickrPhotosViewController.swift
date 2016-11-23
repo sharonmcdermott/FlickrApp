f2ed991d24aef569cec88b758b646b37673b130c
sharon mcdermott <sharonmcdermott@me.com>
2016-11-23 11:25:44 -0500

    Set the class for the collection view to FlickrPhotosViewController.

--- FlickrApp/FlickrPhotosViewController.swift
+++ FlickrApp/FlickrPhotosViewController.swift
@@ -11,4 +11,5 @@ import UIKit
 final class FlickrPhotosViewController: UICollectionViewController {
     // MARK: - Properties
     fileprivate let reuseidentifier = "FlickrCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
 }
